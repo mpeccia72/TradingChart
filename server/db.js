e47c5d53095c81ef8f51d9c1941dc5e134eda402
Michael Peccia <113073047+mpeccia72@users.noreply.github.com>
2024-12-09 23:31:58 -0600

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const pool = new Pool({
     user: 'lebronjames',
-    password: "gloriousking!",
-    host: "trading-chart-webapp-db.cvkkg84qkvgj.us-east-2.rds.amazonaws.com",
+    password: redact
+    host: redact
     port: 5432,
     database: "lebronjames",
     ssl: true
